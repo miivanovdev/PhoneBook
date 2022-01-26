7ece3555ad828449659f1871672e71b89c64dc3e
miivanovdev <okudoku@mail.ru>
2022-01-26 04:55:40 +0300

    ContactImage

--- PhoneBook/Models/Contact.cs
+++ PhoneBook/Models/Contact.cs
@@ -18,5 +18,7 @@ namespace PhoneBook.Models
         public string Address { get; set; }    
 
         public string Description { get; set; }
+
+        public string Image { get; set; }
     }
 }
