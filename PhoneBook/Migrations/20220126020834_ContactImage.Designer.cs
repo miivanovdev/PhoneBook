5632938e14f748bd849d54060d7c052102b2a251
miivanovdev <okudoku@mail.ru>
2022-01-26 05:17:07 +0300

    ContactImage migration

--- PhoneBook/Migrations/20220126020834_ContactImage.Designer.cs
+++ PhoneBook/Migrations/20220126020834_ContactImage.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PhoneBook.Models;
 
 namespace PhoneBook.Migrations
 {
     [DbContext(typeof(PhoneBookContext))]
-    partial class PhoneBookContextModelSnapshot : ModelSnapshot
+    [Migration("20220126020834_ContactImage")]
+    partial class ContactImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
